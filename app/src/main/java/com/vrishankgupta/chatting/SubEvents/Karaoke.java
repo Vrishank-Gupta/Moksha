d474f313743f2ded459ce6a83c828b166bcac3b0
vrishank <vrishank98@gmail.com>
2018-03-19 17:03:39 +0530

    Added new

---
+++ app/src/main/java/com/vrishankgupta/chatting/SubEvents/Karaoke.java
@@ -0,0 +1,50 @@
+package com.vrishankgupta.chatting.SubEvents;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.vrishankgupta.chatting.HomeActivity;
+import com.vrishankgupta.chatting.R;
+
+public class Karaoke extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.karaoke_detail);
+
+        ImageView signOut,homeIcon;
+        signOut = (ImageView) findViewById(R.id.sign_out_icon);
+        homeIcon = (ImageView) findViewById(R.id.home_icon);
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AuthUI.getInstance().signOut(Karaoke.this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getApplicationContext(),"Signed Out",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+            }
+        });
+
+        homeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                finish();
+            }
+        });
+    }
+}
